444733c8ff30b3501c78430b467eb96b7fab8962
Mahaba <mahaba.008@gmail.com>
2024-01-31 15:01:14 +0300

    print statement

--- src/lab2/Lab2.java
+++ src/lab2/Lab2.java
@@ -16,6 +16,7 @@ public class Lab2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello 251 course");
     }
     
 }
